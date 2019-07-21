f2e39e5c809f1abb6305c45e2ce58084cdb6043b
Grigorii Horos <horosgrisa@gmail.com>
2019-07-22 02:51:22 +0300

    Change prefix

--- genSymbols.js
+++ genSymbols.js
@@ -4,5 +4,5 @@ const figures = require('figures');
 
 Object.entries(figures)
   .map(([key, value]) => {
-    console.log(`export ${changeCase.constantCase(key)}='${value}'`);
+    console.log(`export ZSH_${changeCase.constantCase(key)}='${value}'`);
   });
