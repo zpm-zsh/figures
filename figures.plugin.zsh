f2241d546ba39613b91b3f4c2b550c9ff799b987
Grigorii Horos <horosgrisa@gmail.com>
2019-11-08 18:26:39 +0200

    Fix $0

--- figures.plugin.zsh
+++ figures.plugin.zsh
@@ -1,3 +1,7 @@
 #!/usr/bin/env zsh
+# Standarized $0 handling, following:
+# https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+_DIRNAME="${0:h}"
 
-source "${0:h}/symbols.zsh"
+source "${_DIRNAME}/symbols.zsh"
